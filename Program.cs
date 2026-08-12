2d505995768fd824fe5c2ae98d5044d83daf4feb
steinerr12 <fernandodpaz12@gmail.com>
2026-08-12 17:22:53 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        
+        //NIVEL 1: CENTRO DE DECISIONES :v.
+
+        //Calculador de recursos.
+
+    }
+}
